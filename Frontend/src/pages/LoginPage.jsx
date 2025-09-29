c35940cdaf955a009541be02b76617d1b829708b
Sujan Kumar K <sujan.22cs164@sode-edu.in>
2025-09-29 18:16:01 +0530

    Update LoginPage.jsx

--- Frontend/src/pages/LoginPage.jsx
+++ Frontend/src/pages/LoginPage.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 
-const API_URL = 'http://localhost:5000/api/users/';
+const API_URL = 'https://ieee-student-branch-backend.onrender.com/api/users/';
 
 const LoginPage = () => {
     const [formData, setFormData] = useState({
